998eaa1635d6d4157419b0381a1e98da161678b0
sofijandrew <soan3921@colorado.edu>
2022-04-28 13:00:27 -0600

    Update master-game.js

--- js/master-game.js
+++ js/master-game.js
@@ -1,9 +1,6 @@
 
 //make circles disappear when clicked
-///option 1
 
-
-////////option 2
 function circleClicked() {
   document.getElementById('blue-circle').classList.toggle('was-clicked');
 }
@@ -18,8 +15,8 @@ function circleClicked() {
 function windowLoaded() {
   console.log('loaded');
   document.getElementById('blue-circle').addEventListener('click', circleClicked);
-  document.getElementById('orange-circle').addEventListener('mouseover', circleClicked);
-  document.getElementById('red-button').addEventListener('mousedown', circleClicked);
+  document.getElementById('orange-circle').addEventListener('click', circleClicked);
+  document.getElementById('red-button').addEventListener('click', circleClicked);
 }
 
 window.onload = windowLoaded;
